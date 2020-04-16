17236e17ba207f04ad3481f65cda0731455e1287
Amber Williams <awilliams1234@live.com>
2020-04-16 17:15:42 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -44,11 +44,10 @@ class Student
   end
 
   def self.new_from_db(row)
-    new_student = Student.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
+     id = row[0]
+     name = row[1]
+     grade = row[2]
+     Student.new(name, grade, id)
   end
 
   def self.find_by_name(name)
