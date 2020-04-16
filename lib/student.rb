59c9233c8abca6dc9b6292d5cc1e8319801526c9
Amber Williams <awilliams1234@live.com>
2020-04-16 17:16:47 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -54,8 +54,7 @@ class Student
     sql = <<-SQL
     SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
+    DB[:conn].execute(sql, name)
     end
   end
 
