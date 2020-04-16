5a54232898dd84f3406eb1eb6f6043e5b40e15bb
Amber Williams <awilliams1234@live.com>
2020-04-16 14:46:54 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -11,6 +11,20 @@ class Student
     @id = id
   end
 
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade TEXT)
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
+  end 
+
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
