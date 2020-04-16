97fbd00f4c902b48c213e3fb2c16864de6ff2329
Amber Williams <awilliams1234@live.com>
2020-04-16 14:51:12 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -23,7 +23,19 @@ class Student
     DROP TABLE students
     SQL
     DB[:conn].execute(sql)
-  end 
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+      INSERT INTO students (name, grade) VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
 
 
   # Remember, you can access your database connection anywhere in this class
